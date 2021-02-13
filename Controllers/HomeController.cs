15d1b1d632ac5a0e42a02e462a54fba031270883
Igor <fpv555@gmail.com>
2021-02-13 23:07:09 +0500

    Added admin panel password

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using ASPtask.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASPtask.Controllers
 {
@@ -27,7 +23,22 @@ namespace ASPtask.Controllers
         {
             return View();
         }
-
+        public IActionResult AdminPasswordCheck(string pass)
+        {
+            if (pass != null)
+            using (System.Security.Cryptography.SHA512 shaM = new System.Security.Cryptography.SHA512Managed())
+            {
+                string hashedpass = string.Empty;
+                var hash = shaM.ComputeHash(System.Text.Encoding.UTF8.GetBytes(pass));
+                var hashToString = new System.Text.StringBuilder(128);
+                foreach (var @byte in hash) hashToString.Append(@byte.ToString("X2"));
+                hashedpass = hashToString.ToString();
+                if (hashedpass != null && hashedpass == "B48327F35BCB9A8D1352819E8686B8ADBFB3F9A91A23F1463CDDFEB416EF3321E9E4D896522BF2E0237E11D6261622B1DDC2C9F98BFD906110BD11BFADA8A299")
+                    return RedirectToAction("AdminPanel", "Home");
+                else return View();
+            }
+            else return View();
+        }
         public IActionResult About()
         {
             return View();
